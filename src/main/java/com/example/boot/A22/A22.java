34c5982c8c42c3312b51318522d66a2771bab286
zds <1529219484@qqcom>
2024-09-05 09:21:07 +0800

    fix: 启动报错

--- src/main/java/com/example/boot/A22/A22.java
+++ src/main/java/com/example/boot/A22/A22.java
@@ -7,15 +7,15 @@ import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
 public class A22 {
     public static void main(String[] args) throws NoSuchMethodException {
         // 1. 反射获取参数名
-        Method foo = Bean2.class.getMethod("foo", String.class, int.class);
+//        Method foo = Bean2.class.getMethod("foo", String.class, int.class);
 //        for (Parameter parameter : foo.getParameters()) {
 //            System.out.println(parameter.getName());
 //        }
 
         // 2. 基于本地变量表来获取
-        LocalVariableTableParameterNameDiscoverer discoverer = new LocalVariableTableParameterNameDiscoverer();
-        String[] parameterNames = discoverer.getParameterNames(foo);
-        System.out.println(Arrays.toString(parameterNames));
+//        LocalVariableTableParameterNameDiscoverer discoverer = new LocalVariableTableParameterNameDiscoverer();
+//        String[] parameterNames = discoverer.getParameterNames(foo);
+//        System.out.println(Arrays.toString(parameterNames));
 
     }
 }
